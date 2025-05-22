c3ca22a119afc8420c09387fb6f205caaef240bf
SvitlanaOseichuk <141953748+SvitlanaOseichuk@users.noreply.github.com>
2025-05-22 13:09:52 +0300

    add render

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import fetch from 'node-fetch';
 
 dotenv.config(); 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
+
+app.use(cors());
 app.use(express.static('public'));
 
 app.get('/api/currencies', async (req, res) => {
